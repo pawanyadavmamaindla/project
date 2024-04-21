9dbf007b2956c02bbc75c314365c58bf759d3506
pawanyadavmamindla <pawanyadavmamindla18@gmail.com>
2024-04-21 16:10:36 +0530

    first commit

--- backend/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGOURI:"mongodb+srv://pawanyadavmamindla18:y9oZ4ZcMWPl1rxFU@cluster0.zle1y7j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    JWT_SECRET:"abcgdhhfhi"
-}
