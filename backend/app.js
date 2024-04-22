5ae42ccfd8bc72c2e30529bc1c15c37569a436e9
pawanyadavmamindla <pawanyadavmamindla18@gmail.com>
2024-04-22 22:03:18 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,13 @@ const app = express()
 const mongoose  = require('mongoose')
 const PORT = process.env.PORT || 5000
 const {MONGOURI} = require('./config/keys')
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 mongoose.connect(MONGOURI,{
     useNewUrlParser:true,
@@ -17,6 +23,10 @@ mongoose.connection.on('error',(err)=>{
     console.log("err connecting",err)
 })
 
+app.get("/",(req,res) =>{
+    res.json("hello")
+})
+
 require('./models/user')
 require('./models/post')
 
@@ -26,13 +36,7 @@ app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 
 
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static('client/build'))
-    const path = require('path')
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
+
 
 app.listen(PORT,()=>{
     console.log("server is running on",PORT)
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -36,7 +36,7 @@ const SignIn  = ()=>{
             M.toast({html: "invalid email",classes:"#c62828 red darken-3"})
             return
         }
-        fetch("/signup",{
+        fetch("https://pawan-project.onrender.com/signup",{
             method:"post",
             headers:{
                 "Content-Type":"application/json"
